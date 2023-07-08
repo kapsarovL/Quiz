225e44c20042fa48557ff384343f1b23d4ab6187
kapsarovL <kapsarovlazar@gmail.com>
2023-07-08 20:20:46 +0200

    Add files via upload

---
+++ end.js
@@ -0,0 +1,23 @@
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
+saveHighScore = (e) => {
+    e.preventDefault();
+    const score = {
+        score: mostRecentScore,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+};
+
+
